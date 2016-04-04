3b43c29553f7c96118754e98e88affe7953d9847
Omneya Khaled <omnyakhaled5811@gmail.com>
2016-04-04 23:31:22 +0200

    all files

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var express       = require('express');
+var app           = express();
+
+app.use(express.static('public'));
+// require('./routes')(app);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function () {
+	console.log('up');
+});
+// module.exports = app;
---
+++ public/controllers/script.js
@@ -0,0 +1,8 @@
+var scotchApp = angular.module('scotchApp', []);
+
+    
+    scotchApp.controller('mainController', function($scope) {
+
+     
+        $scope.message = 'Everyone come and see how good I look!';
+    });
