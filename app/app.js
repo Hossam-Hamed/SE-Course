dbfa012e4e1373875331aaa33adde57d052a38e5
Menna-elshahawy <menna.elshahawy95@gmail.com>
2016-04-16 13:04:35 +0200

    creating services

--- app/app.js
+++ app/app.js
@@ -1,14 +1,25 @@
 var express       = require('express');
 var app           = express();
+var bodyParser    = require('body-parser');
+var mongo 	= require('./db');
+
+// Export environment vars first thing
+// require('dotenv').load();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use(express.static('public'));
 // require('./routes')(app);
 
-app.get('/', function(req, res) {
-	res.sendFile('index.html');
-});
+// app.get('/', function(req, res) {
+// 	res.sendFile('index.html');
+// });
+require('./routes')(app);
 
 app.listen(3000, function () {
 	console.log('up');
+	mongo.init('mongodb://localhost:27017/flights_db');
+
 });
-// module.exports = app;
+module.exports = app;
