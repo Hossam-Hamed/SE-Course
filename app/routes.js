ac7452419b41475d36e36fb42afb01349cfd994e
Sarah Aboelseoud <sarah.aboelseoud@gmail.com>
2016-04-19 10:53:27 +0200

    final

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
- module.exports = function(app) {
-   var seats;
+module.exports = function(app) {
+
     var mongo = require('../db.js'); 
      var moment = require('moment');
     var fs = require('fs');
@@ -8,13 +8,12 @@
        var codes =  require('./dummy data/flight.json');
        res.json( codes );
     });
-
-
     app.get('/db/seed',function(callback) {
-
 
             mongo.db().collection('Flights').count(function(err, count) {
                 if (count == 0) {
+
+
                     var routes = JSON.parse(fs.readFileSync('flights.json', 'utf8'));
                     for (var i = 0; i < routes.length; i++) {
                         var route = routes[i];
@@ -31,17 +30,19 @@
                         // loop until May 31 2016 starting today April-15-2016
                         for (var i = 1; i <= 46; i++) {
                             doc = {
+
                                 "flightNumber": flight.flightNumber,
                                 "aircraft": flight.aircraft,
                                 "capacity": flight.capacity,
-                                "date": moment().add(i, 'days').calendar(),
+                                "Arrivaldate": moment().add(i, 'days').calendar(),
+                                "DepartureDate": moment().add(i, 'days').calendar(),
+
                                 //"date"          : new Date(),
                                 "duration": flight.duration,
                                 "origin": _origin,
                                 "destination": _destination,
                                 "seatmap": []
                             };
-
 
                             mongo.db().collection('Flights').insert(doc, function(err, data) {
                                 if (err) console.log('error');
@@ -53,7 +54,7 @@
                     }
                 }
             })
-             var airports = JSON.parse(fs.readFileSync('airports.json', 'utf8'));
+ var airports = JSON.parse(fs.readFileSync('airports.json', 'utf8'));
             mongo.db().collection('Airports').count(function(err, count) {
                 // assert.equal(err, null);
                 if (count == 0) {
@@ -98,38 +99,4 @@
         });
 
 
-  })
-    app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:class' 
-  ,function(req,res){
-
-   mongo.db().collection('Flights').find({'origin':req.origin ,'destination': req.destination
-,'departingDate' : req.departingDate,'returningDate' : req.returningDate, 'cabin' : req.cabin})
-   .toArray().then(function (flights) {
-   console.log("heloo");
-   res.send(flights);
- });
-})
-  app.get('/api/flights/search/:origin/:destination/:departingDate/:class'
-  ,function(req,res){
-  mongo.db.collection('Flights').find({'origin' :  req.origin,'destination' :req.destination,
-'departingDate' : req.departingDate,'cabin' : req.cabin})
-   .toArray().then(function (flights){
-  console.log("heloo");
-   res.send(flights);
- });
-})
-
-
-app.get('/api/baalabezoo/:from/:to/:flightDate/:cabin', function(req, res) {
- seats = mongo.db().collection('balbezoo').find().toArray(function(err, docs) {
-        cb(err, docs);
-    })
-  });
-
-exports.seats;
-
-
-
-
-};
-
+  })};
