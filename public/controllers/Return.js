c3800a53bcc78d8977c1bea8296548aa51affb4a
Menna-elshahawy <menna.elshahawy95@gmail.com>
2016-04-19 12:53:29 +0200

    fixing the outgoing and return controllers

--- public/controllers/Return.js
+++ public/controllers/Return.js
@@ -46,13 +46,15 @@
 // 	}
 // }
 // })
-App.controller('return',function($scope){
-    $scope.departure='IAD';
-    $scope.arrival = 'Alex';
-    $scope.departureairport = 'Alex';
-    $scope.departuretime = '3:00';
-    $scope.arrivalairport = 'IAD';
-    $scope.arrivaltime = '4:00';
-    $scope.stops = 0;
-    $scope.tranzit = 'no stops';
+App.controller('return',function($scope,mainSrv){
+    // $scope.departure='IAD';
+    // $scope.arrival = 'Alex';
+    // $scope.departureairport = 'Alex';
+    // $scope.departuretime = '3:00';
+    // $scope.arrivalairport = 'IAD';
+    // $scope.arrivaltime = '4:00';
+    // $scope.stops = 0;
+    // $scope.tranzit = 'no stops';
+	  $scope.flight =mainSrv.getx();
+
 })
