33ed2a43fbc4a1cbe6f9878a4a204e1fa8719d7b
Hoss554 <hossamayman554@gmail.com>
2016-04-19 10:27:29 +0200

    missing http post only

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,20 @@
 module.exports = function(app) {
 
 
-app.get('/api/sum/:x/:y', function(req, res) {
-	var x = parseInt(req.params.x);
-	var y = parseInt(req.params.y);
-	res.send((x + y) + '');
-});
+	app.get('/api/sum/:x/:y', function(req, res) {
+		var x = parseInt(req.params.x);
+		var y = parseInt(req.params.y);
+		res.send((x + y) + '');
+	});
 
 
-app.get('/', function(req, res) {
-res.sendFile('index.html');
-});
+	app.get('/', function(req, res) {
+		res.sendFile('index.html');
+	});
 
+	app.post('/api/postReserv/', data)
+	.success(function(data, status) {
+		mongo.db().collection('reservation').insert(data);
 
-
-
+	});
 };
