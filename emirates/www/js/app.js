17b2aeb7f09efc339973f2b54a4eb0bc07987a91
Omneya Khaled <omnyakhaled5811@gmail.com>
2016-05-02 14:11:37 +0200

    routes of contact us and history

--- emirates/www/js/app.js
+++ emirates/www/js/app.js
@@ -47,17 +47,17 @@ App.config(function($stateProvider, $urlRouterProvider) {
         controller: 'landingCtrl'
       }
     }
-  });
+  })
 
-  // .state('tab.chats', {
-  //     url: '/chats',
-  //     views: {
-  //       'tab-chats': {
-  //         templateUrl: 'templates/tab-chats.html',
-  //         controller: 'ChatsCtrl'
-  //       }
-  //     }
-  //   })
+  .state('tab.history', {
+      url: '/history',
+      views: {
+        'history': {
+          templateUrl: 'templates/history.html',
+          // controller: 'ChatsCtrl'
+        }
+      }
+    })
   //   .state('tab.chat-detail', {
   //     url: '/chats/:chatId',
   //     views: {
@@ -68,15 +68,15 @@ App.config(function($stateProvider, $urlRouterProvider) {
   //     }
   //   })
 
-  // .state('tab.account', {
-  //   url: '/account',
-  //   views: {
-  //     'tab-account': {
-  //       templateUrl: 'templates/tab-account.html',
-  //       controller: 'AccountCtrl'
-  //     }
-  //   }
-  // });
+  .state('tab.contactUs', {
+    url: '/contactUs',
+    views: {
+      'contactUs': {
+        templateUrl: 'templates/contuctus.html',
+        
+      }
+    }
+  });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/landing');
